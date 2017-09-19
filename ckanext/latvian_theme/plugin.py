204398234a135fffa15b2151c31e631f68eb9697
CRImier <crimier@yandex.ru>
2017-09-20 01:12:59 +0300

    Initial user_list auth function

--- ckanext/latvian_theme/plugin.py
+++ ckanext/latvian_theme/plugin.py
@@ -11,3 +11,23 @@ class Latvian_ThemePlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'latvian_theme')
+
+
+class Latvian_AuthPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IAuthFunctions)
+    
+    def auth_user_list(self, context, data_dict):
+        user = context["model"].User.get(context.get('user'))
+        #ignore_auth is used in email sending code
+        if context.ignore_auth is True:
+            return {'success': True}
+        #sysadmin can see anything
+        elif user.sysadmin:
+            return {'success': True}
+        #all else fails
+        else:
+            return {'success': False}
+
+    #IAuthFunctions
+    def get_auth_functions(self):
+        return {"user_list": self.auth_user_list}
