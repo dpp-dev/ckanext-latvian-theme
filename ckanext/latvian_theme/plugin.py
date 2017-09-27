9112bb10666b77d20bd82154fb2a75988dcab7cb
root <root@dpp.dsistemas.lv>
2017-09-28 01:59:40 +0300

    Fixed user_show restrictions

--- ckanext/latvian_theme/plugin.py
+++ ckanext/latvian_theme/plugin.py
@@ -13,32 +13,35 @@ class Latvian_ThemePlugin(plugins.SingletonPlugin):
         toolkit.add_resource('fanstatic', 'latvian_theme')
 
 
+def auth_user_list(context, data_dict):
+    user = context["model"].User.get(context.get('user'))
+    #ignore_auth is used in email sending code
+    if context.get('ignore_auth') is True:
+        return {'success': True}
+    #sysadmin can see anything
+    elif getattr(user, 'sysadmin', False):
+        return {'success': True}
+    #all else fails
+    else:
+        return {'success': False}
+    
+@toolkit.auth_allow_anonymous_access
+def auth_user_show(context, data_dict):
+    if not context.get("for_view", False) and not context.get("api_version", False):
+        #Not in a view and not through the API, likely to be internal code
+        return {"success":True}
+    #Either view or API request
+    user = context.get('user', '')
+    viewed_user = getattr(context.get("user_obj"), "name", None)
+    if user == viewed_user:
+        return {'success': True}
+    else:
+        return auth_user_list(context, data_dict)
+
 class Latvian_AuthPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IAuthFunctions)
-    
-    def auth_user_list(self, context, data_dict):
-        user = context["model"].User.get(context.get('user'))
-        #ignore_auth is used in email sending code
-        if context.get('ignore_auth') is True:
-            return {'success': True}
-        #sysadmin can see anything
-        elif user.sysadmin:
-            return {'success': True}
-        #all else fails
-        else:
-            return {'success': False}
-
-
-    def auth_user_show(self, context, data_dict):
-        user = context.get('user', '')
-        viewed_user = data_dict["id"]
-        if user == viewed_user:
-            return {'success': True}
-        else:
-            return self.auth_user_list(context, data_dict)
-
     #IAuthFunctions
     def get_auth_functions(self):
-        return {"user_list": self.auth_user_list,
-                "user_show": self.auth_user_show
+        return {"user_list": auth_user_list,
+                "user_show": auth_user_show
                }
