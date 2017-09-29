4a42568e0e888463f27d58934985152e03ad7211
root <root@dpp.dsistemas.lv>
2017-09-29 10:39:10 +0300

    Added activity list hiding from non-sysadmin users

--- ckanext/latvian_theme/plugin.py
+++ ckanext/latvian_theme/plugin.py
@@ -7,6 +7,14 @@ import ckan.plugins as plugins
 from ckan.common import _, c, request
 import ckan.plugins.toolkit as toolkit
 
+from logic import check_access
+
+import ckan.logic.action.get as original_actions
+
+#Function to get original actions
+get_original_action = lambda name: getattr(original_actions, name)
+
+#Getting the original revision controller
 from ckan.controllers.revision import RevisionController
 
 class Latvian_ThemePlugin(plugins.SingletonPlugin):
@@ -97,6 +105,13 @@ def auth_user_show(context, data_dict):
     else:
         return auth_user_list(context, data_dict)
 
+def auth_activity_streams(context, data_dict):
+    user = context.get("auth_user_obj", {})
+    if not getattr(user, 'sysadmin', False):
+        return {"success": False}
+    else:
+        return {"success": True}
+
 #Overridden actions
 
 #CKAN install I'm working on is too old to have action chaining available
@@ -106,16 +121,50 @@ def auth_user_show(context, data_dict):
 #    print(data_dict)
 #    return datastore_search(context, data_dict)
 
+@logic.side_effect_free
+def activity_action_replace(action_name, silent_error_return_value):
+    @logic.side_effect_free
+    def wrapper(context, data_dict):
+        try:
+            check_access("activity_streams", context, data_dict)
+            original_action = get_original_action(action_name)
+            return original_action(context, data_dict)
+        except logic.NotAuthorized:
+            #The proper way
+            #Does not work
+            return silent_error_return_value
+        except Exception as e:
+            #No idea why such tricks should be necessary, but it doesn't work the proper way
+            if type(e).__name__ == "NotAuthorized":
+                return silent_error_return_value
+            else:
+                raise
+    return wrapper
+
 class Latvian_AuthPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IAuthFunctions)
     plugins.implements(plugins.IRoutes, inherit=True)
-    #plugins.implements(plugins.IActions)
+    plugins.implements(plugins.IActions)
 
     #IActions
-    #def get_actions(self):
-    #    print("action added")
-    #    return {"datastore_search":safe_datastore_search}
+    def get_actions(self):
+        #return {"datastore_search":safe_datastore_search}
+        actions = [ ["user_activity_list", []],
+                    ["user_activity_list_html", ""],
+                    ["package_activity_list", []],
+                    ["package_activity_list_html", ""],
+                    ["recently_changed_packages_activity_list", []],
+                    ["recently_changed_packages_activity_list_html", ""],
+                    ["group_activity_list", []],
+                    ["group_activity_list_html", ""],
+                    ["organization_activity_list", []],
+                    ["organization_activity_list_html", ""] 
+                  ]
+        replaced_actions = {action: activity_action_replace(action, value)
+                            for action, value in actions}
+        return replaced_actions
     
+    #IRoutes
     def before_map(self, map):
         rev_c = 'ckanext.latvian_theme.plugin:CustomRevisionController'
         map.connect('/revision', controller=rev_c, action='index')
@@ -130,6 +179,7 @@ class Latvian_AuthPlugin(plugins.SingletonPlugin):
         return {"user_list": auth_user_list,
                 "group_show": auth_group_show,
                 #"datastore_search": auth_datastore_search,
+                "activity_streams": auth_activity_streams,
                 "organization_show": auth_group_show,
                 "user_show": auth_user_show
                }
